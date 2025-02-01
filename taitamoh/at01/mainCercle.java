3134bb7ec913a6fe6073688506ff6f9f0315208a
mohamed abddoulaye taita <2386567@cmontmorency.qc.ca>
2025-02-01 11:46:56 -0500

    maincercle

--- taitamoh/at01/mainCercle.java
+++ taitamoh/at01/mainCercle.java
@@ -1,5 +1,15 @@
 package taitamoh.at01;
 
+import java.util.Scanner;
+
 public class mainCercle {
-    
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+cercle c1 = new cercle(9,2,5);
+c1.afficher();
+cercle c2 = new cercle(0,0,7);
+c2.afficher();
 }
+}
+
