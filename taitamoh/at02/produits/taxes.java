4cba61c12259ff7c98adcf70a930846ffd23cef9
mohamed abddoulaye taita <2386567@cmontmorency.qc.ca>
2025-02-06 11:08:26 -0500

    c

---
+++ taitamoh/at02/produits/taxes.java
@@ -0,0 +1,8 @@
+public class taxes {
+    public static final double TAUX_TPS=0.05;
+    public static final double TAUX_TVQ=0.09975;
+    private static double taxeTps;
+    private static double taxeTvq;
+    private static double totalTaxes;
+
+}
